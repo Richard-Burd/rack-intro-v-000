30f77e79b1069472d05e391a61285d740f6e850e
Richard Burd <richardburdx@yahoo.com>
2017-11-22 22:28:12 +0000

    automatically backed up by learn

--- application.rb
+++ application.rb
@@ -3,7 +3,7 @@ class Application
   def call(env)
     resp = Rack::Response.new
     resp.write "Hello, my name is Something"
-    resp.write "\nThe time right now is: #{Time.now}"
+    resp.write "\nThe time right now is: #{Time.now}\n\n"
 
     num_1 = Kernel.rand(1..3)
     num_2 = Kernel.rand(1..3)
