3f5a449311df01210d2ee6bdc00165bb531d74ec
Richard Burd <richardburdx@yahoo.com>
2017-11-22 21:23:53 +0000

    automatically backed up by learn

--- application.rb
+++ application.rb
@@ -3,7 +3,7 @@ class Application
   def call(env)
     resp = Rack::Response.new
     resp.write "Hello, my name is Something"
-    resp.write "\n The time right now is: #{Time.now}"
+    resp.write "\nThe time right now is: #{Time.now}"
     resp.finish
   end
 
