260b62186cb45400a3748cdc0dff869cbbfd40b7
Richard Burd <richardburdx@yahoo.com>
2017-11-22 21:51:44 +0000

    automatically backed up by learn

--- application.rb
+++ application.rb
@@ -5,6 +5,16 @@ class Application
     resp.write "Hello, my name is Something"
     resp.write "\nThe time right now is: #{Time.now}"
     resp.finish
+
+    num_1 = Kernel.rand(1..20)
+    num_2 = Kernel.rand(1..20)
+    num_3 = Kernel.rand(1..20)
+ 
+    if num_1==num_2 && num_2==num_3
+      puts "You Win"
+    else
+      puts "You Lose"
+    end
   end
 
 end
