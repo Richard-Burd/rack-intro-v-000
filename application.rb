57fe6b59b003223c75632624810272c7cfa9b52c
Richard Burd <richardburdx@yahoo.com>
2017-11-22 22:27:53 +0000

    automatically backed up by learn

--- application.rb
+++ application.rb
@@ -8,9 +8,9 @@ class Application
     num_1 = Kernel.rand(1..3)
     num_2 = Kernel.rand(1..3)
 
-    resp.write "#{num_1}\n"
-    resp.write "#{num_2}\n"
-    
+    resp.write "\n#{num_1}\n"
+    resp.write "\n#{num_2}\n"
+
     if num_1==num_2
       resp.write "\n\n\nYou Win"
     else
